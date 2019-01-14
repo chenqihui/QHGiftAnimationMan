58e944ce46fd12777608c0241645c18953762acf
anakinChen <qihuichen@sohu-inc.com>
2019-01-15 01:03:13 +0800

    【Add】

--- QHGiftAnimationMan/ViewController.swift
+++ QHGiftAnimationMan/ViewController.swift
@@ -30,15 +30,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMatrisAction(_ sender: Any) {
-        let widthView: CGFloat = QHRandomLocation.getRandomNumer(from: 12, to: 22)
-        
-        let image = UIImage.init(named: QHRandomLocation.getRandomNumer(from: 0, to: 1) == 0 ? "cao" : "hua")
-        let subImageView = UIImageView.init(frame: CGRect(x: 0, y: 0, width: widthView, height: widthView))
-        subImageView.image = image
+//        let widthView: CGFloat = QHRandomLocation.getRandomNumer(from: 12, to: 22)
+//        let image = UIImage.init(named: QHRandomLocation.getRandomNumer(from: 0, to: 1) == 0 ? "cao" : "hua")
+//        let subImageView = UIImageView.init(frame: CGRect(x: 0, y: 0, width: widthView, height: widthView))
+//        subImageView.image = image
         
 //        matrisManager!.addMatrisAnimation(name: "xiaolian2", centerPoint: contentView.center, subView: subImageView)
         
-        matrisManager!.addMatrisAnimationRandomLocation(name: QHRandomLocation.getRandomNumer(from: 0, to: 1) == 0 ? "xiaolian" : "diao", subView: subImageView)
+//        matrisManager!.addMatrisAnimationRandomLocation(name: QHRandomLocation.getRandomNumer(from: 0, to: 1) == 0 ? "xiaolian" : "diao", subView: subImageView)
+        
+        let widthView: CGFloat = 10
+        let image = UIImage.init(named: "hua")
+        let subImageView = UIImageView.init(frame: CGRect(x: 0, y: 0, width: widthView, height: widthView))
+        subImageView.image = image
+        matrisManager!.addMatrisAnimation(name: "ttt", centerPoint: CGPoint(x: UIScreen.main.bounds.size.width/2, y: UIScreen.main.bounds.size.height/2), subView: subImageView)
+        //        matrisManager!.addMatrisAnimationRandomLocation(name: "ttt", subView: subImageView)
     }
     
 }
